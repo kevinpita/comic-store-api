e5e9aa8feb7de8d99a31802a8a072cd29909c711
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-04-26 01:09:45 +0200

    feat(service): create comic creator service

---
+++ src/main/java/io/github/kevinpita/comicstoreapi/comiccreator/ComicCreatorService.java
@@ -0,0 +1,29 @@
+/* Kevin Pita 2022 */
+package io.github.kevinpita.comicstoreapi.comiccreator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ComicCreatorService {
+    private final ComicCreatorRepository comicCreatorRepository;
+
+    public ComicCreatorService(ComicCreatorRepository comicCreatorRepository) {
+        this.comicCreatorRepository = comicCreatorRepository;
+    }
+
+    public ComicCreator save(ComicCreator comicCreator) {
+        return comicCreatorRepository.save(comicCreator);
+    }
+
+    public ComicCreator findById(Long id) {
+        return comicCreatorRepository.findById(id).orElse(null);
+    }
+
+    public Iterable<ComicCreator> findAll() {
+        return comicCreatorRepository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        comicCreatorRepository.deleteById(id);
+    }
+}
