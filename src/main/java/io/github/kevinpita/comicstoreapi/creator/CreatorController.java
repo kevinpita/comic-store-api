fb917b17bc347e208c0ad9ba5e2427846c7e9fc6
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-04-26 01:10:31 +0200

    feat(controller): create creator controller

---
+++ src/main/java/io/github/kevinpita/comicstoreapi/creator/CreatorController.java
@@ -0,0 +1,13 @@
+/* Kevin Pita 2022 */
+package io.github.kevinpita.comicstoreapi.creator;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreatorController {
+    private final CreatorRepository creatorRepository;
+
+    public CreatorController(CreatorRepository creatorRepository) {
+        this.creatorRepository = creatorRepository;
+    }
+}
