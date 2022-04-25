155fb7f90d7f7ea3e149b5306fd0ad519cb5104e
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-04-25 23:21:04 +0200

    feat(filter): create basic request filter

---
+++ src/main/java/io/github/kevinpita/comicstoreapi/configuration/SecurityFilter.java
@@ -0,0 +1,19 @@
+/* Kevin Pita 2022 */
+package io.github.kevinpita.comicstoreapi.configuration;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
