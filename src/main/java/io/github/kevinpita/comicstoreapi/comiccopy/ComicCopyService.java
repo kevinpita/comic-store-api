45e00dfc9fd406e3d8a52bb903b1cb04785cecd0
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-04-26 01:08:57 +0200

    feat(service): create comic copy service

---
+++ src/main/java/io/github/kevinpita/comicstoreapi/comiccopy/ComicCopyService.java
@@ -0,0 +1,25 @@
+/* Kevin Pita 2022 */
+package io.github.kevinpita.comicstoreapi.comiccopy;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ComicCopyService {
+    private final ComicCopyRepository comicCopyRepository;
+
+    public ComicCopyService(ComicCopyRepository comicCopyRepository) {
+        this.comicCopyRepository = comicCopyRepository;
+    }
+
+    public ComicCopy create(ComicCopy comicCopy) {
+        return comicCopyRepository.save(comicCopy);
+    }
+
+    public ComicCopy update(ComicCopy comicCopy) {
+        return comicCopyRepository.save(comicCopy);
+    }
+
+    public void delete(Long id) {
+        comicCopyRepository.deleteById(id);
+    }
+}
