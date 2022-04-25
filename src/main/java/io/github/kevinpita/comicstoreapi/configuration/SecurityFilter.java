dff954e82b825753f03acba0bfe5fead156c7538
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-04-26 00:09:34 +0200

    feat(filter): make filter reject requests when auth token is not valid

--- src/main/java/io/github/kevinpita/comicstoreapi/configuration/SecurityFilter.java
+++ src/main/java/io/github/kevinpita/comicstoreapi/configuration/SecurityFilter.java
@@ -2,18 +2,39 @@
 package io.github.kevinpita.comicstoreapi.configuration;
 
 import java.io.IOException;
+import java.util.Objects;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Component
+@Slf4j
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Value("${VALID_AUTH_TOKEN:valid_token}")
+    private String validAuthToken;
 
     @Override
     protected void doFilterInternal(
             HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        boolean isApiCall = request.getRequestURI().startsWith("/api/");
+        String requestAuthToken = request.getHeader("Authorization");
+        log.info(
+                String.format(
+                        "Request URI: %s. Request Auth Token: %s",
+                        request.getRequestURI(), requestAuthToken));
+
+        if (isApiCall && !Objects.equals(requestAuthToken, validAuthToken)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            return;
+        }
+
         filterChain.doFilter(request, response);
     }
 }
