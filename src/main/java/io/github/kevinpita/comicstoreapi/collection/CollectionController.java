0358e83a7ecbab2fdc5b1a8378e4aadd16970a21
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-04-26 17:12:48 +0200

    refactor(collection-controller): pluralize collections endpoint

--- src/main/java/io/github/kevinpita/comicstoreapi/collection/CollectionController.java
+++ src/main/java/io/github/kevinpita/comicstoreapi/collection/CollectionController.java
@@ -6,7 +6,7 @@ import javax.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/collection")
+@RequestMapping("/api/v1/collections")
 public class CollectionController {
     private final CollectionService collectionService;
 
