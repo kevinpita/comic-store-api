eb612d2df88a24f811920bbbeb88e2965ae2c7fb
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-06-08 17:39:47 +0200

    feat(comic-controller): create comic and get all comics

--- src/main/java/io/github/kevinpita/comicstoreapi/comic/ComicController.java
+++ src/main/java/io/github/kevinpita/comicstoreapi/comic/ComicController.java
@@ -1,13 +1,26 @@
 /* Kevin Pita 2022 */
 package io.github.kevinpita.comicstoreapi.comic;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/comics")
 public class ComicController {
     private final ComicService comicService;
 
     public ComicController(ComicService comicService) {
         this.comicService = comicService;
+    }
+
+    @GetMapping
+    public List<Comic> getComics() {
+        return comicService.getAllComics();
+    }
+
+    @PostMapping
+    public Comic createComic(@Valid @RequestBody Comic comic) {
+        return comicService.createComic(comic);
     }
 }
