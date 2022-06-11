0b68a8c3cf40b2bb8d2ae6750ee6c5baec190d20
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-06-12 00:35:36 +0200

    feat(image): create invalid model id and model not found exceptions

---
+++ src/main/java/io/github/kevinpita/comicstoreapi/image/exception/InvalidModelIdException.java
@@ -0,0 +1,8 @@
+/* Kevin Pita 2022 */
+package io.github.kevinpita.comicstoreapi.image.exception;
+
+public class InvalidModelIdException extends RuntimeException {
+    public InvalidModelIdException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/github/kevinpita/comicstoreapi/image/exception/ModelNotFoundException.java
@@ -0,0 +1,8 @@
+/* Kevin Pita 2022 */
+package io.github.kevinpita.comicstoreapi.image.exception;
+
+public class ModelNotFoundException extends RuntimeException {
+    public ModelNotFoundException(String message) {
+        super(message);
+    }
+}
