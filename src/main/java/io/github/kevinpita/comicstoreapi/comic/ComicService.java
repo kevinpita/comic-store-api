3fc3b8b480ae6e4837c615a3777fa3cf4a2e9fc6
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-06-12 12:02:51 +0200

    feat(comic): make comic service transform model into dto

--- src/main/java/io/github/kevinpita/comicstoreapi/comic/ComicService.java
+++ src/main/java/io/github/kevinpita/comicstoreapi/comic/ComicService.java
@@ -1,7 +1,11 @@
 /* Kevin Pita 2022 */
 package io.github.kevinpita.comicstoreapi.comic;
 
+import io.github.kevinpita.comicstoreapi.comiccopy.ComicCopy;
+import io.github.kevinpita.comicstoreapi.comiccreator.ComicCreatorDto;
+import io.github.kevinpita.comicstoreapi.comiccreator.ComicCreatorService;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,5 +26,40 @@ public class ComicService {
 
     public List<Comic> getAllComics() {
         return comicRepository.findAll();
+    }
+
+    public static ComicDto from(Comic comic) {
+        ComicDto.CollectionDto collection =
+                ComicDto.CollectionDto.builder()
+                        .id(comic.getCollection().getId())
+                        .name(comic.getCollection().getName())
+                        .build();
+
+        List<ComicCreatorDto> comicCreators =
+                comic.getComicCreators().stream()
+                        .map(ComicCreatorService::from)
+                        .collect(Collectors.toList());
+
+        List<ComicDto.ComicCopyDto> copies =
+                comic.getCopies().stream().map(ComicService::fromCopy).collect(Collectors.toList());
+
+        return ComicDto.builder()
+                .id(comic.getId())
+                .title(comic.getTitle())
+                .description(comic.getDescription())
+                .issueNumber(comic.getIssueNumber())
+                .collection(collection)
+                .comicCreators(comicCreators)
+                .copies(copies)
+                .build();
+    }
+
+    public static ComicDto.ComicCopyDto fromCopy(ComicCopy comicCopy) {
+        return ComicDto.ComicCopyDto.builder()
+                .id(comicCopy.getId())
+                .purchaseDate(comicCopy.getPurchaseDate())
+                .state(comicCopy.getState())
+                .price(comicCopy.getPrice())
+                .build();
     }
 }
